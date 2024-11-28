94fe597f7f998aed57119ab5dce5c05c8465aead
Yulia Sukhareva <93522379+Suharichek@users.noreply.github.com>
2024-11-28 20:03:19 +0300

    Add files via upload

--- BetterYOU/BetterYOU/MainViews/BetterSleep.swift
+++ BetterYOU/BetterYOU/MainViews/BetterSleep.swift
@@ -5,6 +5,7 @@
 //  Created by Сухарик on 22.11.2024.
 //
 
+import CoreML
 import SwiftUI
 
 struct BetterSleep: View {
@@ -107,17 +108,24 @@ struct BetterSleep: View {
     }
     
     func calculateBedtime() -> String {
-        
+        do {
+            let config = MLModelConfiguration()
+            let model = try SleepCalculator(configuration: config)
+            
             let components = Calendar.current.dateComponents([.hour, .minute], from: wakeUp)
             let hour = (components.hour ?? 0) * 60 * 60
             let minute = (components.minute ?? 0) * 60
             
-            let prediction = Double(hour + minute) + sleepAmount * 5 + Double(coffeeAmount) * 15
+            let prediction = try model.prediction(wake: Double(hour + minute), estimatedSleep: sleepAmount, coffee: Double(coffeeAmount), workHours: Double(workAmount))
             
-            let sleepTime = wakeUp - prediction
+            let sleepTime = wakeUp - prediction.actualSleep
             
             return sleepTime.formatted(date: .omitted, time: .shortened)
+        } catch {
+            return "Error"
+        }
     }
+
 }
 
 #Preview {
